537e575ff64d5e56e9c3d18237cb9ef38e478316
NamrataKarmalkar <namratakarmalkar2111@gmail.com>
2024-04-15 15:50:54 +0530

    first commit

---
+++ DemoRegAndLoginWithIdentity/Controllers/PatientsController.cs
@@ -0,0 +1,171 @@
+﻿
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DemoRegAndLoginWithIdentity.Models;
+using global::DemoRegAndLoginWithIdentity.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoRegAndLoginWithIdentity.Controllers
+{
+    public class PatientsController : Controller
+    {
+        private readonly MyDbContext _context;
+
+        public PatientsController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: Patients
+        [Authorize(Roles = "Admin,Patients")]
+        public async Task<IActionResult> Index()
+        {
+            return _context.Patients != null ?
+                        View(await _context.Patients.ToListAsync()) :
+                        Problem("Entity set 'AppDbContext.Patients'  is null.");
+        }
+
+        [Authorize(Roles = "Admin,Patients")]
+        // GET: Patients/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Patients == null)
+            {
+                return NotFound();
+            }
+
+            var patient = await _context.Patients
+                .FirstOrDefaultAsync(m => m.PatientID == id);
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            return View(patient);
+        }
+
+        [Authorize(Roles = "Admin")]
+        // GET: Patients/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Patients/Create
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PatientId,TestName,Price")] Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(patient);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(patient);
+        }
+
+        [Authorize(Roles = "Admin")]
+        // GET: Patients/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Patients == null)
+            {
+                return NotFound();
+            }
+
+            var patient = await _context.Patients.FindAsync(id);
+            if (patient == null)
+            {
+                return NotFound();
+            }
+            return View(patient);
+        }
+
+        [Authorize(Roles = "Admin")]
+        // POST: Patients/Edit/5
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PatientID,TestName,Price")] Patient patient)
+        {
+            if (id != patient.PatientID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(patient);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PatientExists(patient.PatientID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(patient);
+        }
+
+        [Authorize(Roles = "Admin")]
+        // GET: Patients/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Patients == null)
+            {
+                return NotFound();
+            }
+
+            var patient = await _context.Patients
+                .FirstOrDefaultAsync(m => m.PatientID == id);
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            return View(patient);
+        }
+
+        [Authorize(Roles = "Admin")]
+        // POST: Patients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Patients == null)
+            {
+                return Problem("Entity set 'AppDbContext.Patients'  is null.");
+            }
+            var patient = await _context.Patients.FindAsync(id);
+            if (patient != null)
+            {
+                _context.Patients.Remove(patient);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PatientExists(int id)
+        {
+            return (_context.Patients?.Any(e => e.PatientID == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ DemoRegAndLoginWithIdentity/Models/Patient.cs
@@ -0,0 +1,20 @@
+﻿
+//using System.ComponentModels;
+namespace DemoRegAndLoginWithIdentity.Models
+
+ //namespace Pathlab_Dotnet.Models
+{
+    public class Patient
+    {
+        public int PatientID { get; set; }
+ 
+        public string TestName { get; set; }
+
+        public int Price { get; set; }
+
+        
+
+
+
+    }
+}
